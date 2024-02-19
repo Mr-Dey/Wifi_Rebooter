bd1669d58dde34c31a3980d1068ffba6559b3d8c
mr-dey <subhasishdeycoc@gmail.com>
2024-02-19 09:19:50 +0530

    Updated with more details

--- main.py
+++ main.py
@@ -1,44 +1,55 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
 
+#to catch the page source if needed.
 def catch(html):
     with open("page1.html","w", encoding="utf-8") as x:
         x.write(html)
+        
+def my_tplink():
+    driver=webdriver.Chrome()
 
-driver=webdriver.Chrome()
-router_address="http://192.168.0.3/"
+    #Confidential
+    router_address="http://192.168.0.3/"
+    username="admin"
+    password="admin123"
 
-password_user_id="userName"
-password_field_id="pcPassword"
-login_btn_id="loginBtn"
-# menu_btn_id="menu_tools"
-# reboot_btn0="menu_restart"
-reboot_btn_id="button_reboot"
-user="admin"
-password="admin123"
+    #loadpage
+    driver.get(router_address)
+    driver.implicitly_wait(10)
 
-driver.get(router_address)
-driver.implicitly_wait(10)
+    #ids
+    user_id="userName"
+    password_id="pcPassword"
+    login_btn_id="loginBtn"
+    system_tools_menu_id="menu_tools"
+    reboot_menu_id="menu_restart"
+    reboot_btn_id="button_reboot"
 
 
-login_feild_user=driver.find_element(By.ID, password_user_id)
-login_feild_pass=driver.find_element(By.ID, password_field_id)
-login_btn=driver.find_element(By.ID,login_btn_id)
-login_feild_pass.send_keys(password)
+    #locateids
+    login_feild_user=driver.find_element(By.ID, user_id)
+    login_feild_pass=driver.find_element(By.ID, password_id)
+    login_btn=driver.find_element(By.ID,login_btn_id)
+    
+    #fill
+    # login_feild_user.send_keys(username) 
+    login_feild_pass.send_keys(password)
 
-login_btn.click()
-driver.switch_to.frame("frame1")
-menu_btn=driver.find_element(By.ID,"menu_tools")
-menu_btn.click()
-menu_restart=driver.find_element(By.ID,"menu_restart")
-menu_restart.click()
-driver.switch_to.parent_frame()
-driver.switch_to.frame("frame2")
-reboot=driver.find_element(By.ID,"button_reboot")
-reboot.click()
-time.sleep(5)
+    #automation
+    login_btn.click()
+    driver.switch_to.frame("frame1")
+    menu_btn=driver.find_element(By.ID,system_tools_menu_id)
+    menu_btn.click()
+    menu_restart=driver.find_element(By.ID,reboot_menu_id)
+    menu_restart.click()
+    driver.switch_to.parent_frame()
+    driver.switch_to.frame("frame2")
+    reboot_btn=driver.find_element(By.ID,reboot_btn_id)
+    reboot_btn.click()
+    alart=driver.switch_to.alert
+    alart.accept()
+    print("Rebooting sucessfully")
 
-# catch(driver.page_source)
+
+my_tplink()
