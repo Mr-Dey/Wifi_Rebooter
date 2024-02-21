47131ef4df8e7476b7c94e726f599418a7f27421
mr-dey <subhasishdeycoc@gmail.com>
2024-02-21 09:15:40 +0530

    updated the main

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-#to catch the page source if needed.
+
+class Tplink_Archer_C20:
+    def __init__(self,gateway,username,password):
+        self.geteway=f"http://{gateway}/"
+        self.username=username
+        self.password=password
+    user_id="userName"
+    password_id="pcPassword"
+    login_btn_id="loginBtn"
+    system_tools_menu_id="menu_tools"
+    reboot_menu_id="menu_restart"
+    reboot_btn_id="button_reboot"
+
+
 def catch(html):
     with open("page1.html","w", encoding="utf-8") as x:
         x.write(html)
@@ -12,47 +25,78 @@ def my_tplink():
     option.add_argument("-headless")
     driver=webdriver.Chrome(options=option)
 
-    #Confidential
-    router_address="http://192.168.0.3/"
-    username="admin"
-    password="admin123"
-
+    tplink_home=Tplink_Archer_C20(gateway="192.168.0.3",username="admin",password="admin123")
     #loadpage
-    driver.get(router_address)
+    driver.get(tplink_home.geteway)
     driver.implicitly_wait(10)
 
-    #ids
-    user_id="userName"
-    password_id="pcPassword"
-    login_btn_id="loginBtn"
-    system_tools_menu_id="menu_tools"
-    reboot_menu_id="menu_restart"
-    reboot_btn_id="button_reboot"
-
-
-    #locateids
-    login_feild_user=driver.find_element(By.ID, user_id)
-    login_feild_pass=driver.find_element(By.ID, password_id)
-    login_btn=driver.find_element(By.ID,login_btn_id)
+    try:
+        userName=driver.find_element(By.ID, tplink_home.user_id)
+        userName.send_keys(tplink_home.username)
+    except Exception as e:
+        print("username Exception! Trying to reboot.")
+    passWord=driver.find_element(By.ID, tplink_home.password_id)
+    passWord.send_keys(tplink_home.password)
+    loginBtn=driver.find_element(By.ID,tplink_home.login_btn_id)
+    loginBtn.click()
+    print("Login sucessful")
     
-    #fill
-    # login_feild_user.send_keys(username) 
-    login_feild_pass.send_keys(password)
-
-    #automation
-    login_btn.click()
     driver.switch_to.frame("frame1")
-    menu_btn=driver.find_element(By.ID,system_tools_menu_id)
+    menu_btn=driver.find_element(By.ID,tplink_home.system_tools_menu_id)
     menu_btn.click()
-    menu_restart=driver.find_element(By.ID,reboot_menu_id)
+    menu_restart=driver.find_element(By.ID,tplink_home.reboot_menu_id)
     menu_restart.click()
     driver.switch_to.parent_frame()
     driver.switch_to.frame("frame2")
-    reboot_btn=driver.find_element(By.ID,reboot_btn_id)
+    reboot_btn=driver.find_element(By.ID,tplink_home.reboot_btn_id)
     reboot_btn.click()
     alart=driver.switch_to.alert
     alart.accept()
-    print("Rebooting sucessfully")
+    print("Rebooting")
+
+
+def parent_tplink():
+    username=input("Enter usename :")
+    password=input("Enter password :")
+    router=Tplink_Archer_C20(gateway="192.168.0.1",username=username,password=password)
+    
+    #headlessmode
+    options=Options()
+    options.add_argument("-headless") 
+    driver=webdriver.Chrome(options=options)
+    driver.implicitly_wait(10)
+    driver.get(router.geteway)
+    
+    #input feild
+    try:
+        userName=driver.find_element(By.ID,router.user_id)
+        userName.send_keys(username)
+        pcPassword=driver.find_element(By.ID,router.password_id)
+        pcPassword.send_keys(password)
+        loginBtn=driver.find_element(By.ID,router.login_btn_id)
+        loginBtn.click()
+        print("Logged in.")
+    except Exception as e:
+        print(e)
+    # frame1
+    driver.switch_to.frame('frame1')
+    system_tools=driver.find_element(By.ID,router.system_tools_menu_id)
+    system_tools.click()
+    reboot_menu=driver.find_element(By.ID,router.reboot_menu_id)
+    reboot_menu.click()
+    driver.switch_to.parent_frame()
+    
+    #frame2
+    driver.switch_to.frame('frame2')
+    reboot_btn=driver.find_element(By.ID,router.reboot_btn_id)
+    reboot_btn.click()
+    
+    #alart
+    reboot_alart=driver.switch_to.alert
+    reboot_alart.accept()
+    print("Rebooting")
+    
+    
 
 def digisol():
     option=Options()
@@ -91,11 +135,8 @@ def digisol():
     print('Process digisol Complete!')
 
 
-def temp():
-    print("Will replace this code with parent router of home tp link")
-
 if __name__=="__main__":
-    func ={'0':my_tplink,'1':temp,'2':digisol}
+    func ={'0':my_tplink,'1':parent_tplink,'2':digisol}
     userInp=input("""
                   enter '0' for home
                   enter '1' for home_parent
