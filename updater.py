6877d2bfa200a2857f7389ce06e74cc147b0ca88
mr-dey <subhasishdeycoc@gmail.com>
2024-02-21 09:33:37 +0530

    updated updater

--- updater.py
+++ updater.py
@@ -1,7 +1,13 @@
 import subprocess
-subprocess.run("pip freeze > requirements.txt")
+import os
+try:
+    subprocess.run("pip freeze > requirements.txt")
+except:
+    subprocess.run("pip3.12 freeze > requirements.txt")
+    
 with open("requirements.txt","r") as req:
     x=req.read()
+    os.remove("requirements.txt")
 names=[names.split("==")[0] for names in x.splitlines()]
 
 for x in range(len(names)):
